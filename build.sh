8f15d42f9c408e37c270170535c40e272879933f
LesVu <johndie005@gmail.com>
2024-04-07 18:05:36 +0800

    Up

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # docker build . -f Dockerfile --no-cache --build-arg="user=abc" --tag lesvu/vn_vnc:latest
-docker build . -f Dockerfile --build-arg="user=abc" --tag lesvu/android-studio_vnc:latest
+docker build . -f Dockerfile --build-arg="user=abc" --tag lesvu/android-studio:latest
 # docker push lesvu/vn_vnc:latest
